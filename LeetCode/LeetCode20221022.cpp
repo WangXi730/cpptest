7d71136af9ba1784938fd2755b7522a2319d6aa8
WangXi730 <wangxi730403730@163.com>
2022-10-22 19:35:05 +0800

    .

--- LeetCode/LeetCode20221022.cpp
+++ LeetCode/LeetCode20221022.cpp
@@ -27,12 +27,32 @@ public:
         }
         return dp[s.size()][p.size()];
     }
+//45. 跳跃游戏 II
+    int jump(vector<int>& nums) {
+        //dp[i]表示这个位置最少需要几步
+        vector<int> dp(nums.size(), -1);
+        //初始位置为0，所以dp[0] = 0;
+        dp[0] = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] + i +1>= nums.size()) {
+                return dp[i] + 1;
+            }
+            for (int j = i + 1; j < nums.size() && j - i <= nums[i]; ++j) {
+                if (dp[j] == -1 || dp[j] > dp[i] + 1) {
+                    dp[j] = dp[i] + 1;
+                }
+            }
+        }
+        return dp[nums.size() - 1];
+    }
 };
 
 
 
 int main() {
     Solution test;
-    test.isMatch("abefcdgiescdfimde", "ab*cd?i*de");
+    //test.isMatch("abefcdgiescdfimde", "ab*cd?i*de");
+    vector<int> tmp{ 2, 3, 1, 1, 4 };
+    test.jump(tmp);
     return 0;
 }
