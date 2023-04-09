36dad9e34d76466b586ff41a30eb57b83c8fb101
WangXi730 <wangxi730403730@163.com>
2023-04-09 16:22:49 +0800

    Update 2023-4-9.cpp

--- LeetCode/2023-4-9.cpp
+++ LeetCode/2023-4-9.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<unordered_set>
 using namespace std;
 
 class Solution {
@@ -18,12 +19,95 @@ public:
         }
         return res;
     }
+public:    
+    vector<string> removeInvalidParentheses(string s) {
+    bool res_cond = false;
+    vector<string> res;
+    int lr = 0;
+    int c_num = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == '(') {
+            ++lr;
+        }
+        else if(s[i] == ')') {
+            --lr;
+        }
+        else {
+            ++c_num;
+        }
+    }
+    for (int rmnum = max(0, abs(lr)); rmnum < s.size() - c_num + 1; ++rmnum) {
+        if (res_cond) {
+            return res;
+        }
+        unordered_set<string> uset;
+        string ts = "";
+        dfs(uset, s, ts, 0, rmnum, lr);
+        for (auto& e : uset) {
+            if (valid(e)) {
+                res_cond = true;
+                res.push_back(e);
+            }
+        }
+    }
+    if (res.empty()) {
+        return { "" };
+    }
+    return res;
+    }
+private:
+    void dfs(unordered_set<string>& res, string& s, string& ts, int i, int rm, int lr) {
+        if (rm == 0) {
+            if (lr != 0) {
+                return;
+            }
+            if (i == s.size()) {
+                res.insert(ts);
+            }
+            else {
+                res.insert(ts + s.substr(i));
+            }
+            return;
+        }
+        else if (i >= s.size()) {
+            return;
+        }
+        else if (abs(lr) > rm) {
+            return;
+        }
+        int lr_d = 0;
+        if (s[i] == '(') {
+            lr_d = -1;
+        }
+        else if (s[i] == ')') {
+            lr_d = 1;
+        }
+        dfs(res, s, ts, i + 1, rm - 1, lr+lr_d);
+        ts.push_back(s[i]);
+        dfs(res, s, ts, i + 1, rm,lr);
+        ts.pop_back();
+    }
+    //ÅÐ¶ÏÊÇ·ñÊÇÓÐÐ§À¨ºÅ×Ö·û´®
+    bool valid(const string& s) {
+        int left_stack = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (left_stack < 0) {
+                return false;
+            }
+            else if (s[i] == '(') {
+                ++left_stack;
+            }
+            else if (s[i] == ')') {
+                --left_stack;
+            }
+        }
+        return left_stack == 0;
+    }
 };
 
 
 int main() {
     Solution test;
-    vector<int> a {10, 9, 2, 5, 3, 7, 101, 18};
-    test.lengthOfLIS(a);
+    test.removeInvalidParentheses("()())()");
     return 0;
 }
