d7d8e1b5729a66732d41c578032a51ac38f93d04
WangXi730 <wangxi730403730@163.com>
2023-09-13 10:20:48 +0800

    Update 2023-9-13.cpp

--- LeetCode/2023-9-13.cpp
+++ LeetCode/2023-9-13.cpp
@@ -7,47 +7,39 @@ public:
         int m = grid[0].size();
         int row = 0;
         int col = 0;
-        for (int i = 0; i < n * m; ++i) {
+        for (int i = 0; i < n * m - 1; ++i) {
             //Âí×ßÈÕ
             if (row + 2 < n && col + 1 < m && grid[row + 2][col + 1] == 1 + i) {
                 row = row + 2;
                 col = col + 1;
-                ++i;
             }
             else if (row + 2 < n && col - 1 >= 0 && grid[row + 2][col - 1] == 1 + i) {
                 row = row + 2;
                 col = col - 1;
-                ++i;
             }
             else if (row - 2 >= 0 && col + 1 < m && grid[row - 2][col + 1] == 1 + i) {
                 row = row - 2;
                 col = col + 1;
-                ++i;
             }
             else if (row - 2 >= 0 && col - 1 >= 0 && grid[row - 2][col - 1] == 1 + i) {
                 row = row - 2;
                 col = col - 1;
-                ++i;
             }
             else if (row + 1 < n && col + 2 < m && grid[row + 1][col + 2] == 1 + i) {
                 row = row + 1;
                 col = col + 2;
-                ++i;
             }
             else if (row + 1 < n && col - 2 >= 0 && grid[row + 1][col - 2] == 1 + i) {
                 row = row + 1;
                 col = col - 2;
-                ++i;
             }
             else if (row - 1 >= 0 && col + 2 < m && grid[row - 1][col + 2] == 1 + i) {
                 row = row - 1;
                 col = col + 2;
-                ++i;
             }
             else if (row - 1 >= 0 && col - 2 >= 0 && grid[row - 1][col - 2] == 1 + i) {
                 row = row - 1;
                 col = col - 2;
-                ++i;
             }
             else {
                 return false;
@@ -62,6 +54,6 @@ public:
 int main() {
     vector<vector<int>> v{{0, 11, 16, 5, 20}, { 17,4,19,10,15 }, { 12,1,8,21,6 }, { 3,18,23,14,9 }, { 24,13,2,7,22 }};
     Solution test;
-    test.checkValidGrid(v);
+    cout << boolalpha << test.checkValidGrid(v);
     return 0;
 }
