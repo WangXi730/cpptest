f0dcf31be781f4b9fc8bd9dae41b6c6c008d445a
WangXi730 <wangxi730403730@163.com>
2023-09-15 15:53:20 +0800

    Update 2023-9-15.cpp

--- LeetCode/2023-9-15.cpp
+++ LeetCode/2023-9-15.cpp
@@ -6,27 +6,20 @@ using namespace std;
 class Solution {
 public:
     vector<int> countPairs(int n, vector<vector<int>>& edges, vector<int>& queries) {
-        vector<int> link(edges.size(), 1);
-        for (int i = 0; i < edges.size(); ++i) {
-            for (int j = i + 1; j < edges.size(); ++j) {
-                if (edges[i][0] == edges[j][0] || edges[i][0] == edges[j][1] || edges[i][1] == edges[j][0] || edges[i][1] == edges[j][1]) {
-                    ++link[i];
-                    ++link[j];
-                }
+        map<int, multiset<int>> mp;
+        for (int i = 0; i < edges.size(); ++i)
+        {
+            mp[edges[i][0]].insert(edges[i][1]);
+            mp[edges[i][1]].insert(edges[i][0]);
+
+        }
+        vector<int> hash(n + 1,0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int times = mp[i].size() + mp[j].size() - mp[i].count(j);
+                ++hash[times];
             }
         }
-        vector<int> hash(edges.size() + 1, 0);
-        for (int i = 0; i < hash.size(); ++i) {
-            ++hash[link[i]];
-        }
-        for (int i = 1; i < hash.size(); ++i) {
-            hash[i] += hash[i - 1];
-        }
-        vector<int> answers(queries.size(), 0);
-        for (int i = 0; i < queries.size(); ++i) {
-            answers[i] = hash[queries[i]];
-        }
-        return answers;
     }
 };
 
